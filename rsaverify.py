8471cc023a55217054852320f39b61f62fea8358
Damien Zammit <damien@zamaudio.com>
2013-06-28 06:22:06 +1000

    Test cases.

--- rsaverify.py
+++ rsaverify.py
@@ -11,8 +11,10 @@ filesig = open("rsa_sig", "rb")
 fileraw = open("verifythis3", "rb")
 
 e = 17L
-n = bytes_to_long(filen.read(0x100)[::-1])
-sig = filesig.read(0x100)[::-1]
+#n = bytes_to_long(filen.read(0x100)[::-1])
+n = bytes_to_long(filen.read(0x100))
+#sig = filesig.read(0x100)[::-1]
+sig = filesig.read(0x100)
 
 #length = 0x25a000-0x958  #verifythis
 #length = 0x25a000-0x890  #verifythis2
@@ -45,17 +47,17 @@ print "Digest: "+digest.hexdigest()
 
 sha1digest = SHA1.new(digest.hexdigest())
 
-if verifier1.verify(sha1digest, sig):
+if verifier1.verify(digest, sig):
 	print "Verified SHA1+SHA256 PKCS1 1.5 OK"
-if verifier2.verify(sha1digest, sig):
+if verifier2.verify(digest, sig):
 	print "Verified SHA1+SHA256 PSS_8 OK"
-if verifier3.verify(sha1digest, sig):
+if verifier3.verify(digest, sig):
 	print "Verified SHA1+SHA256 PSS_1 OK"
-if verifier4.verify(sha1digest, sig):
+if verifier4.verify(digest, sig):
 	print "Verified SHA1+SHA256 PSS_0 OK"
-if verifier5.verify(sha1digest, sig):
+if verifier5.verify(digest, sig):
 	print "Verified SHA1+SHA256 PSS_20 OK"
-if verifier6.verify(sha1digest, sig):
+if verifier6.verify(digest, sig):
 	print "Verified SHA1+SHA256 PSS_17 OK"
 
 filen.close()
